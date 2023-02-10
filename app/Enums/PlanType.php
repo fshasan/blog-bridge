b1833161593f4f94d0ced6d3539c00566a6274f8
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2023-02-10 20:19:47 +0600

    made enums for post limit & plan type for users.

---
+++ app/Enums/PlanType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+interface PlanType
+{
+    const FREE = 1;
+    const PREMIUM = 2;
+}
---
+++ app/Enums/PostsPerDay.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+interface PostsPerDay
+{
+    const FREE_USER = 2;
+}
