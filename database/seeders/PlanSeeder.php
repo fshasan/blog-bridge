cdb11b5383b27f3a3ce5d53ec2b4b389b77c1d18
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2023-02-10 16:01:38 +0600

    added: db seed and boileplate for membership plans (free, premium)

---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Plan;
+
+class PlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $plans = [
+            [
+                'name' => 'Free Plan', 
+                'slug' => 'free-plan', 
+                'stripe_plan' => 'price_1MZJZGCSkADIfETSEpthV6Gv', 
+                'price' => 0, 
+                'description' => 'This is a free membership plan. Free members will be able to create 2 posts daily.'
+            ],
+            [
+                'name' => 'Premium Plan', 
+                'slug' => 'premium-plan', 
+                'stripe_plan' => 'price_1MZKPmCSkADIfETSGC99rc37', 
+                'price' => 10, 
+                'description' => 'This is premium membership plan. Premium members will be able to create unlimited posts.'
+            ]
+        ];
+
+        foreach ($plans as $plan) {
+            Plan::create($plan);
+        }
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,6 +19,9 @@
                     <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
                         {{ __('Posts') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('membership.plan')" :active="request()->routeIs('membership.plan')">
+                        {{ __('Purchase Plan') }}
+                    </x-nav-link>
                 @else
                     
                 @endif
