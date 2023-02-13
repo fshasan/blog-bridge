ad2468f2a2db5771490f54b1e05f63d4f4a45d1f
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2023-02-13 17:15:05 +0600

    added: load of user info from cache.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 use App\Enums\UserType;
+use App\Enums\CacheTime;
 
 class UserController extends Controller
 {
     public function getUsers()
     {
-        $users = User::latest()->where('is_admin', UserType::USER)->paginate();
-
+        $users = Cache::remember('user-info', CacheTime::CACHE_FOR_A_MINUTE, function() {
+            return User::where('is_admin', UserType::USER)->latest()->paginate();
+        });
+        
         return view('dashboard', compact('users'));
     }
 }
