f56865235fd14dd2f935aef24e11fa68eeecd315
Farhan Sharukh Hasan <sharukh75hasan@gmail.com>
2023-02-13 20:12:02 +0600

    updated: user my plans drop down responsive

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -105,6 +105,9 @@
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('membership.my-plan')">
+                    {{ __('My Plans') }}
+                </x-responsive-nav-link>
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
